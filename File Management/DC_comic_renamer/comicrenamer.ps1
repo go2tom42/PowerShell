fed84a2151a7c067d41c4d845ccb6fee076bc60e
go2tom42 <tom42martin@gmail.com>
2019-04-22 16:06:42 -0400

    Update comicrenamer.ps1

--- File Management/DC_comic_renamer/comicrenamer.ps1
+++ File Management/DC_comic_renamer/comicrenamer.ps1
@@ -1,6 +1,7 @@
 $filelist = Get-ChildItem 'z:\comics\New DC' -file
 
 ForEach ($file in $filelist) {
+    #$newdir = [regex]::match($file.name,'((\d{5}\s)(.*?)(\d{1}))').Groups[3].Value
     #$newdir = [regex]::match($file.name,'((\d{5}\s)(.*?)(\s\d{1}))').Groups[3].Value
     $newdir = [regex]::match($file.name,"((\d{5})(.*?)([^a-z '\-A-Z]))").Groups[3].Value
     $newdir = $file.Directoryname +'\'+$newdir.Trim()
