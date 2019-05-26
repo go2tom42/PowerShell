f39b095d110812af68595dda94318046519f973e
go2tom42 <tom42martin@gmail.com>
2019-05-26 18:14:03 -0400

    fixes

--- File Management/zip to pdf - archive.org/main.ps1
+++ File Management/zip to pdf - archive.org/main.ps1
@@ -1,21 +1,20 @@
-
-
-Write-Host "Analyze Files ..."
+# set these for your setup
 #$magick = 'C:\PROGRA~1\IMAGEM~1.8-Q\magick.exe'
 $magick = 'C:\PROGRA~1\GRAPHI~1.31-\gm.exe'
 $Logfile = ".\errorlog.txt"
-$path = 'y:\media\HDD\Downloads\completed\aria2\knittingreferencelibrary_collection\'
+$path = 'y:\media\HDD\Downloads\completed\aria2\knittingreferencelibrary_collection\missing\'
 $filelist = ".\filelist.txt"
-
-$Files = Get-ChildItem $path -Filter "*.zip"
 $tempfolder = 'c:\scripttest\dump'
 $i = 150
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+Write-Host "Analyze Files ..."
+$Files = Get-ChildItem $path -Filter "*.zip"
 $1 = (($i * 827) / 100)
 $2 = (($i * 1169) / 100)
 
-
 foreach ($File in $Files) {
-    if (Test-path $tempfolder ){
+    if (Test-path $tempfolder ) {
         Remove-Item -Path $tempfolder -Recurse
     }
     $fullPath = $File.FullName;
@@ -37,18 +36,20 @@ foreach ($File in $Files) {
         }
         
         Expand-Archive -Path $fullpath -DestinationPath $tempfolder
-        $ImageFiles = Get-ChildItem $tempfolder | Where-Object { $_.Extension -match 'tif|png|jpg' }
+        $ImageFiles = Get-ChildItem $tempfolder -Recurse | Where-Object { $_.Extension -match 'tif|png|jpg' }
         
         foreach ($Image in $ImageFiles) {
             Add-Content $filelist $Image.FullName
         }
         
-        $illegalchars2 = [string]::join('',([System.IO.Path]::GetInvalidFileNameChars())) -replace '\\','\\'
-        $title = $title -replace "[$illegalchars2]",''
+        $illegalchars2 = [string]::join('', ([System.IO.Path]::GetInvalidFileNameChars())) -replace '\\', '\\'
+        $title = $title -replace "[$illegalchars2]", ''
+        $title = $title[0..50] -join ''
         $name = """$path\$BaseName - $title.pdf"""
 
-
+        Write-Host "Starting magick..."
         Start-Process -FilePath $magick -ArgumentList "convert @$filelist -compress jpeg -quality 70 -verbose -density $ix$i -units PixelsPerInch -resize $1x$2 $name" -NoNewWindow -Wait
+        Write-Host "Magick done..."
         if (Test-Path $filelist ) {
             Remove-Item -Path $filelist -Force
         }
