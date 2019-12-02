21885795a6d9112865bcbdbc9876f0e437664038
tom42 <tom42martin@gmail.com>
2019-12-02 11:59:35 -0500

    Update subtitles.ps1

--- Video/BD to MKV/subtitles.ps1
+++ Video/BD to MKV/subtitles.ps1
@@ -29,19 +29,19 @@ $SubtitleEdit = (Get-IniContent settings.ini).settings.SubtitleEdit
 
 
 if (Test-Path ($Destination + "EPs\subtitles")) {
-    Start-Process -FilePath $SubtitleEdit -ArgumentList "/convert *.sup subrip /fixcommonerrors" -WorkingDirectory "$($Destination)EPs\subtitles"  -wait -NoNewWindow
+    Start-Process -FilePath $SubtitleEdit -ArgumentList "/convert *.sup subrip /removetextforhi /redocasing /fixcommonerrors /reversertlstartend" -WorkingDirectory "$($Destination)EPs\subtitles"  -wait -NoNewWindow
     Remove-Item ($Destination + 'EPs\subtitles\*.sup') -Force  
 }
 if (Test-Path ($Destination + "HD\subtitles")) {
-    Start-Process -FilePath $SubtitleEdit -ArgumentList "/convert *.sup subrip /fixcommonerrors" -WorkingDirectory "$($Destination)HD\subtitles"  -wait -NoNewWindow 
+    Start-Process -FilePath $SubtitleEdit -ArgumentList "/convert *.sup subrip /removetextforhi /redocasing /fixcommonerrors /reversertlstartend" -WorkingDirectory "$($Destination)HD\subtitles"  -wait -NoNewWindow 
     Remove-Item ($Destination + 'HD\subtitles\*.sup') -Force  
 }
 if (Test-Path ($Destination + "SD\subtitles")) {
-    Start-Process -FilePath $SubtitleEdit -ArgumentList "/convert *.sup subrip /fixcommonerrors" -WorkingDirectory "$($Destination)SD\subtitles"  -wait -NoNewWindow 
+    Start-Process -FilePath $SubtitleEdit -ArgumentList "/convert *.sup subrip /removetextforhi /redocasing /fixcommonerrors /reversertlstartend" -WorkingDirectory "$($Destination)SD\subtitles"  -wait -NoNewWindow 
     Remove-Item ($Destination + 'SD\subtitles\*.sup') -Force  
 }
 if (Test-Path ($Destination + "4x3\subtitles")) {
-    Start-Process -FilePath $SubtitleEdit -ArgumentList "/convert *.sup subrip /fixcommonerrors" -WorkingDirectory "$($Destination)4x3\subtitles"  -wait -NoNewWindow 
+    Start-Process -FilePath $SubtitleEdit -ArgumentList "/convert *.sup subrip /removetextforhi /redocasing /fixcommonerrors /reversertlstartend" -WorkingDirectory "$($Destination)4x3\subtitles"  -wait -NoNewWindow 
     Remove-Item ($Destination + '4x3\subtitles\*.sup') -Force  
 }
 
