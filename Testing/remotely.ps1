9f25e7d120ebe2aeeead1fa45921c1187f123ba8
tom42 <tom42martin@gmail.com>
2022-04-07 18:39:07 -0400

    Update remotely.ps1

--- Testing/remotely.ps1
+++ Testing/remotely.ps1
@@ -1,3 +1,5 @@
-(New-Object Net.WebClient).DownloadFile('https://remotely.tom42.pw/Content/Remotely_Installer.exe', (join-path (Get-Location).Path 'Remotely_Installer.exe'))
-(New-Object Net.WebClient).DownloadFile('https://remotely.tom42.pw/Content/Remotely-Win10-x64.zip', (join-path (Get-Location).Path 'Remotely-Win10-x64.zip'))
-Start-Process -FilePath "Remotely_Installer.exe"-ArgumentList '-install -quiet -organizationid "4cdb842b-3104-4d01-83b6-5d117f61550c" -serverurl "https://remotely.tom42.pw" -path "Remotely-Win10-x64.zip"'
+(New-Object Net.WebClient).DownloadFile('https://remotely.tom42.pw/Content/Remotely_Installer.exe', (join-path $env:TEMP 'Remotely_Installer.exe'))
+(New-Object Net.WebClient).DownloadFile('https://remotely.tom42.pw/Content/Remotely-Win10-x64.zip', (join-path $env:TEMP 'Remotely-Win10-x64.zip'))
+Start-Process -FilePath "Remotely_Installer.exe"-ArgumentList "-install -quiet -organizationid `"4cdb842b-3104-4d01-83b6-5d117f61550c`" -serverurl `"https://remotely.tom42.pw`" -path `"Remotely-Win10-x64.zip`"" -WorkingDirectory $env:TEMP 
+Remove-Item -Path (join-path $env:TEMP 'Remotely_Installer.exe')
+Remove-Item -Path (join-path $env:TEMP 'Remotely-Win10-x64.zip')
